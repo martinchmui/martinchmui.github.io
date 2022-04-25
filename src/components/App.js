5f4d795bfd9a531503e72a07c070d5866b2ad624
Martin <martinchmui@hotmail.com>
2022-04-24 21:41:30 -0400

    Initial Commit, Landing page

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPage from './LandingPage';
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/LandingPage.css';
+import github from '../assets/GitHub-Mark-120px-plus.png'
+import resume from '../assets/resume.png'
+
+const LandingPage = () => {
+    return (
+        <div className='landingdiv'>
+            <div className='background'>
+            </div>
+            <div className='header top'>Hello, I'm <span>Martin</span></div>
+            <div className='header bot'>I'm a front end web developer</div>
+            <div className='header'>
+                <a href='https://github.com/martinchmui' target='_blank' rel='noreferrer'>
+                    <img className='logo' src={github} alt='To Github' title='To Github'/>
+                </a>
+                <a href='/src/assets/MARTIN_MUI_resume.pdf' target='_blank' rel='noreferrer'>
+                    <img className='logo' src={resume} alt='Resume' title='Resume'/>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
